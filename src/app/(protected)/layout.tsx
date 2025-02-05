ad542eb8c287dbbae5d3d6b8e2212039984e3255
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-02-05 14:30:30 +0530

    Updated Schema

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -11,6 +11,17 @@ const SidebarLayout = ({ children }: Props) => {
   return (
     <SidebarProvider>
       <AppSidebar />
+      <main className="m-2 w-full">
+        <div className="flex items-center gap-2 rounded-md border border-sidebar-border bg-sidebar p-2 px-4 shadow">
+          <div className="ml-auto"></div>
+          <div className="flex gap-4 rounded-full"></div>
+          <UserButton />
+        </div>
+        <div className="h-4"></div>
+        <div className="h-[calc(100vh-6rem)] overflow-y-scroll rounded-md border-sidebar-border bg-sidebar p-4 shadow">
+          {children}
+        </div>
+      </main>
     </SidebarProvider>
   );
 };
